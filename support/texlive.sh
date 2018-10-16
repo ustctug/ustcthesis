0a7eac6bf6e2a6d23ddc51e978210e1a244049e2
Zeping Lee <zepinglee@gmail.com>
2018-10-16 23:24:19 +0800

    remove titletoc dependency

--- support/texlive.sh
+++ support/texlive.sh
@@ -16,7 +16,7 @@ tlmgr update --self --all −−no−auto−install;
 tlmgr install latexmk l3build \
     fontname fontspec l3kernel l3packages xetex \
     cjk ctex environ ms trimspaces ulem xecjk zhnumber \
-    caption filehook footmisc titlesec unicode-math \
+    caption filehook footmisc unicode-math \
     fandol stix2-otf tex-gyre xits \
     booktabs listings thumbpdf xcolor zapfding \
     algorithm2e ifoddpage relsize;
