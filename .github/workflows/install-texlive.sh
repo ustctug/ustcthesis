38941cfce2e49b0235908913c751eba7fb4743be
Zeping Lee <zepinglee@gmail.com>
2020-03-19 23:41:05 +0800

    Add conflict errors

--- .github/workflows/install-texlive.sh
+++ .github/workflows/install-texlive.sh
@@ -16,8 +16,8 @@ HYPERREF_PKGS="bitset letltxmacro pdfescape pdflscape";
 NOMENCL_PKGS="nomencl koma-script xkeyval";
 
 BIN_PKGS="latexmk l3build";
-REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS caption footmisc notoccite titlesec \
-    unicode-math";
+REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS caption footmisc filehook notoccite \
+    titlesec unicode-math";
 FONT_PKGS="fandol tex-gyre xits";
 EXTRA_PKGS="$ALGORITHM2E_PKGS $HYPERREF_PKGS $NOMENCL_PKGS siunitx";
 DOC_PKGS="booktabs listings xcolor";
