4618946533e679dcf2f1df80eda06f716d63b0f5
Zeping Lee <zepinglee@gmail.com>
2021-12-10 20:13:00 +0800

    Use docker for CI

---
+++ .github/workflows/install-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+export PATH="/tmp/texlive/bin/x86_64-linux:$PATH";
+
+XECJK_PKGS="fontspec ulem xecjk xetex";
+CTEX_PKGS="cjk ctex environ everysel trimspaces zhnumber";
+ALGORITHM2E_PKGS="algorithm2e ifoddpage relsize";
+BIBLATEX_PKGS="biber biblatex biblatex-gb7714-2015 xstring";
+NOMENCL_PKGS="nomencl koma-script xkeyval";
+
+BIN_PKGS="latexmk l3build";
+REQUIRED_PKGS="$XECJK_PKGS $CTEX_PKGS caption footmisc filehook notoccite \
+    multirow titlesec unicode-math";
+FONT_PKGS="fandol tex-gyre xits";
+EXTRA_PKGS="$ALGORITHM2E_PKGS $BIBLATEX_PKGS $NOMENCL_PKGS siunitx";
+DOC_PKGS="booktabs hypdoc listings xcolor";
+
+tlmgr install $BIN_PKGS $REQUIRED_PKGS $FONT_PKGS $EXTRA_PKGS $DOC_PKGS;
